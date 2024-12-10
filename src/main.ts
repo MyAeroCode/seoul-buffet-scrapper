462c7ecaa1e2f10ac5b484009989c81ed1c22b72
Lutz <meteo_meteo@naver.com>
2024-12-10 10:51:31 +0900

    feat: 인쌩맥주 뷔페를 리스트에 추가

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+import { LifeBeerBuffet } from './buffets/LifeBeerBuffet';
 import { InstagramScrapperExecuter } from './core/scrapper/InstagramScrapperExecuter';
 
 async function main() {
-    const targetBuffets: InstagramScrapperExecuter[] = [];
+    const targetBuffets: InstagramScrapperExecuter[] = [new LifeBeerBuffet()];
 
     for (const targetBuffet of targetBuffets) {
         await targetBuffet.execute();
