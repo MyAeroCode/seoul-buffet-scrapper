0a2b08520874243ebcbafeedbbf64e454dcdb13e
Lutz <meteo_meteo@naver.com>
2024-12-10 11:14:39 +0900

    feat: 슬랙 메시지를 구어체로 변경

--- src/core/notifier/SlackNotifier.ts
+++ src/core/notifier/SlackNotifier.ts
@@ -7,7 +7,7 @@ import { NotifyArg, SlackMessagePayload } from './SlackNotifierTypes';
 export class SlackNotifier {
     static async notify(arg: NotifyArg) {
         const payload: SlackMessagePayload = {
-            text: `*[${this.getCurrentDate()}]* :drooling_face: ${arg.buffetAlias}`,
+            text: `*[${this.getCurrentDate()} - ${arg.buffetAlias}]* 한식뷔페 파티 찾아요! 🤤`,
             attachments: [],
         };
 
