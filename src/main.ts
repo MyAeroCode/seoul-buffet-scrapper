3814e119d7a300a7e4856a793c2846376f767f8b
Lutz <meteo_meteo@naver.com>
2024-12-10 10:52:04 +0900

    feat: 오미라식당 뷔페를 리스트에 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { LifeBeerBuffet } from './buffets/LifeBeerBuffet';
 import { InstagramScrapperExecuter } from './core/scrapper/InstagramScrapperExecuter';
 
+import { LifeBeerBuffet } from './buffets/LifeBeerBuffet';
+import { OmiraKitchenBuffet } from './buffets/OmiraKitchenBuffet';
+
 async function main() {
-    const targetBuffets: InstagramScrapperExecuter[] = [new LifeBeerBuffet()];
+    const targetBuffets: InstagramScrapperExecuter[] = [
+        new LifeBeerBuffet(),
+        new OmiraKitchenBuffet(),
+    ];
 
     for (const targetBuffet of targetBuffets) {
         await targetBuffet.execute();
