8760a78d73b828cceb7c8ab38d279eeb934faef7
Lutz <meteo_meteo@naver.com>
2025-03-24 10:56:00 +0900

    feat: 인스타그램 크롤링 시 apify 를 사용하도록 변경

--- src/core/scrapper/InstagramScrapper.ts
+++ src/core/scrapper/InstagramScrapper.ts
@@ -1,30 +1,33 @@
-import puppeteer from 'puppeteer-extra';
-import stealthPlugin from 'puppeteer-extra-plugin-stealth';
+import { ApifyClient } from 'apify-client';
 
-puppeteer.use(stealthPlugin());
+import { ApifyConfig } from '../../config/ApifyConfig';
 
 export class InstagramScrapper {
     static async getRecentImages(username: string): Promise<string[]> {
-        const browser = await puppeteer.launch({
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        });
-        const page = await browser.newPage();
-        await page.goto(`https://www.instagram.com/${username}/`);
-
-        await new Promise((resolve) => setTimeout(resolve, 5000));
-
-        const images = await page.evaluate(function () {
-            return Array.from(document.getElementsByTagName('img'))
-                .map((img) => ({
-                    src: img.src,
-                    alt: img.alt,
-                }))
-                .filter((img) => img.alt.startsWith('Photo by'));
+        const client = new ApifyClient({
+            token: ApifyConfig.apiToken,
         });
 
-        await browser.close();
+        const input = {
+            addParentData: false,
+            directUrls: [`https://www.instagram.com/${username}/`],
+            enhanceUserSearchWithFacebookPage: false,
+            isUserReelFeedURL: false,
+            isUserTaggedFeedURL: false,
+            onlyPostsNewerThan: '1 day',
+            resultsLimit: 5,
+            resultsType: 'posts',
+            searchLimit: 1,
+            searchType: 'hashtag',
+        };
 
-        return images.map((img) => img.src);
+        const instagramScrapperActorId = 'shu8hvrXbJbY3Eb9W';
+        const run = await client.actor(instagramScrapperActorId).call(input);
+
+        const { items } = await client.dataset(run.defaultDatasetId).listItems();
+
+        const imageItems = items.filter((items) => items.type === 'Image');
+
+        return imageItems.map((item) => item.displayUrl as string);
     }
 }
