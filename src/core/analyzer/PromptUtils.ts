2d9ad4d2365e0b6dda88696abdfe43b0d2b42407
Lutz <meteo_meteo@naver.com>
2025-07-04 11:20:15 +0900

    fix: 비중이 약한 메뉴무시 (#22)
    * fix: 프롬프트 개선을 통한, 비중이 약한 메뉴무시
    
    * fix: 불필요한 console.log 제거

--- src/core/analyzer/PromptUtils.ts
+++ src/core/analyzer/PromptUtils.ts
@@ -11,11 +11,7 @@ export function generateTodayPrompt(imageUrls: string[]): CoreMessage[] {
             content: [
                 {
                     type: 'text',
-                    text: '당신은 메뉴 이름이 포함된 이미지 분석가입니다.',
-                },
-                {
-                    type: 'text',
-                    text: `${today} 일자 이외의 메뉴가 포함되어 있을 수 있습니다.`,
+                    text: '당신은 식단표 사진을 분석하여 오늘의 메뉴 이름을 추출하는 전문가입니다.',
                 },
                 {
                     type: 'text',
@@ -23,11 +19,19 @@ export function generateTodayPrompt(imageUrls: string[]): CoreMessage[] {
                 },
                 {
                     type: 'text',
-                    text: `${today} 일자 외의 메뉴는 표시하지 않습니다.`,
+                    text: `${today} 일자 이외의 메뉴가 포함되어 있을 수 있습니다. 이러한 메뉴는 무시합니다.`,
+                },
+                {
+                    type: 'text',
+                    text: `비중이 적은 단순한 반찬 메뉴는 무시합니다. 이러한 메뉴의 예시는 다음과 같습니다: (${WEIGHTLESS_MENU})`,
                 },
                 {
                     type: 'text',
                     text: '각 행의 메뉴이름 처음에는 • 로 시작해주세요.',
+                },
+                {
+                    type: 'text',
+                    text: `성격이 비슷한 메뉴는 한 줄로 묶어서 표시해주세요. 이러한 메뉴의 예시는 다음과 같습니다: (${SIMILAR_MENU})`,
                 },
                 {
                     type: 'text',
@@ -41,3 +45,22 @@ export function generateTodayPrompt(imageUrls: string[]): CoreMessage[] {
         },
     ];
 }
+
+const WEIGHTLESS_MENU: string[] = [
+    '야채류',
+    '채소류',
+    '나물류',
+    '샐러드류',
+    '김치류',
+    '절임류',
+    '음료류',
+
+    '누룽지',
+    '비엔나 소시지 볶음',
+];
+
+const SIMILAR_MENU: string[] = [
+    '양념치킨 + 마늘치킨 + 후라이드치킨 >> "양념/ 마늘/ 후라이드 치킨"',
+    '보쌈 + 보쌈소스 >> "보쌈/ 보쌈소스"',
+    '김치제육볶음 + 온두부 >> "두부/ 김치제육볶음"',
+];
